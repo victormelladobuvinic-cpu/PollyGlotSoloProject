39d1af877032ed122feef07c85abffc96d61cc8a
victormelladobuvinic-cpu <victor.melladobuvinic@gmail.com>
2026-02-25 14:46:56 -0300

    rendering answer

--- index.js
+++ index.js
@@ -1,14 +1,59 @@
+
+
+
 const translateBtn = document.getElementById("translate-btn")
 const phrase = document.getElementById("text-area")
+const langForm = document.getElementById("language-form")
+const divForAnswer = document.getElementById("answer-section")
+
+function checkForm() {
+
+     const selectedLanguage = document.querySelector('input[name="leng"]:checked');
+        if (phrase.value.trim() !== "" && selectedLanguage.value) {
+            translateBtn.disabled = false;
+        } else {
+            translateBtn.disabled = true;
+        }
+}
+
+phrase.addEventListener("input", checkForm);
 
 
+langForm.addEventListener("change", checkForm);
 
 
 addEventListener("click", (e) => {
 
     if (e.target.id === "translate-btn") {
-        e.preventDefault()
+        e.preventDefault();
+        const selectedLanguage = document.querySelector('input[name="leng"]:checked');
+                
+
         console.log(phrase.value)
 
+        if (selectedLanguage) {
+            console.log(selectedLanguage.value)
+        } else {
+            alert("...select a language please")
+        };
+
+        translateBtn.classList.add("hidden")
+        document.querySelector(".option-row").classList.add("hidden")
+        document.getElementById("select-language").classList.add("hidden")
+
+        renderAnswer(phrase.value,selectedLanguage.value)
+        
+
     }
- } )
+ });
+
+ function renderAnswer(phrase, language) {
+    divForAnswer.innerHTML = ` 
+    
+    <div class="result-card">
+            <p><strong>Original:</strong> ${phrase}</p>
+            <p><strong>Idioma:</strong> ${language}</p>
+            <p><em>Traducción en camino...</em> 🦜</p>
+        </div>
+        `
+ }
